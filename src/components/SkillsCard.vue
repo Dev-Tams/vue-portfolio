e5558ac47e25f18e2565d2a83a6e5a176ab7f5f9
Dev-Tams <tammyalemu@gmail.com>
2025-04-17 20:54:24 +0100

    add skill component

---
+++ src/components/SkillsCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <li class="m-4">
+        <img :srcset="srcset" :alt="alt" />
+      <a :href="url" target="_blank" :aria-label="ariaLabel">
+        {{ image }}
+      </a>
+    </li>
+</template>
+
+<script setup>
+defineProps({
+url: String,
+alt: String,
+image: String,
+ariaLabel: String
+})
+</script>
