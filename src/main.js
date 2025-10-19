ab415749fa2c3ecb6f48b3eba41750fac670238c
Dev-Tams <tammyalemu@gmail.com>
2025-10-19 19:59:27 +0100

    updated project info

--- src/main.js
+++ src/main.js
@@ -6,8 +6,6 @@ import App from './App.vue'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
-import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createApp(App).mount('#app')
 
